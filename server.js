d22a8c03466bf6090d4b57175a51563a1656332b
itsJohnDev <johncarloaguilar.pinlac@gmail.com>
2026-06-09 03:31:00 +0800

    allowed origin fix

--- server.js
+++ server.js
@@ -16,7 +16,10 @@ const PORT = process.env.PORT || 8000;
 connectDB();
 
 // CORS Config
-const allowedOrigins = ["http://localhost:3000"];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://thought-nest-ui.vercel.app",
+];
 
 app.use(
   cors({
